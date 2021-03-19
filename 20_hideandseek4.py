7188bf5e46518f16a05ad3ef039784e491f4b4ab
jj <cjdool@naver.com>
2021-03-19 23:18:48 +0900

    completed 20

--- 20_hideandseek4.py
+++ 20_hideandseek4.py
@@ -3,8 +3,10 @@ from collections import deque
 
 N, K = map(int, sys.stdin.readline().split())
 visited = [0] * 100001
+visitedlist = [0] * 100001
 
-queue = deque([N])
+queue = deque()
+queue.append(N)
 visited[N] = 1
 
 while queue:
@@ -12,9 +14,16 @@ while queue:
 
     if x == K:
         print(visited[x]-1)
+        temp = [x]
+        while x != N:
+            x = visitedlist[x]
+            temp.append(x)
+        temp.reverse()
+        print(*temp)
         break
 
-    for nx in [x+1, x-1, 2*x]:
+    for nx in [2*x, x+1, x-1]:
         if 0 <= nx <= 100000 and visited[nx] == 0:
             visited[nx] = visited[x] + 1
+            visitedlist[nx] = x
             queue.append(nx)
