b7623bef5cbaaba2e16b1dc04ff8748019bd74dd
jj <cjdool@naver.com>
2021-04-14 21:00:11 +0900

    6th before optimize

--- 6_nqueen.py
+++ 6_nqueen.py
@@ -1,38 +1,48 @@
 import sys
+import copy
 
 N = int(sys.stdin.readline())
 
 
 def basic_dfs(rootx, rooty):
-    visit = [['x']*N for _ in range(N)]
-    stack = [(rootx, rooty, visit, 0)]
+    answerlist = []
+    stack = [(rootx, rooty, [['x']*N for _ in range(N)], 0)]
     totalcnt = 0
 
     while stack:
-        curx, cury, visited, cnt = stack.pop()
-        if visited[curx][cury] == 'x':
-            cnt += 1
-            if cnt == N:
-                totalcnt += 1
-                continue
-            pos = []
-            for i in range(N):
-                for j in range(N):
-                    if visited[i][j] == 'o':
-                        continue
+        curx, cury, visited2, cnt = stack.pop()
+        visited = copy.deepcopy(visited2)
+        pos = []
+        for i in range(N):
+            for j in range(N):
+                if visited[i][j] == 'o' or visited[i][j] == 'q':
+                    continue
 
-                    if i == curx:
-                        visited[i][j] = 'o'
-                    elif j == cury:
-                        visited[i][j] = 'o'
-                    elif i+j == curx+cury:
-                        visited[i][j] = 'o'
-                    elif i-j == curx-cury:
-                        visited[i][j] = 'o'
-                    else:
+                if i == curx:
+                    visited[i][j] = 'o'
+                elif j == cury:
+                    visited[i][j] = 'o'
+                elif i+j == curx+cury:
+                    visited[i][j] = 'o'
+                elif i-j == curx-cury:
+                    visited[i][j] = 'o'
+                else:
+                    if rootx < i or (rootx == i and rooty < j):
                         pos.append((i, j))
-            for x, y in pos:
-                stack.append((x, y, visited, cnt))
+        visited[curx][cury] = 'q'
+        cnt += 1
+        if cnt == N:
+            flag = True
+            for ans in answerlist:
+                if ans == visited:
+                    flag = False
+                    break
+            if flag:
+                totalcnt += 1
+                answerlist.append(visited)
+            continue
+        for x, y in pos:
+            stack.append((x, y, visited, cnt))
 
     return totalcnt
 
