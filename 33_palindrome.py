20ec736861f1446d5f7ffbacabf4dfe732503752
jj <cjdool@naver.com>
2021-04-17 16:15:28 +0900

    new 33~42th

---
+++ 33_palindrome.py
@@ -0,0 +1,7 @@
+import sys
+
+N = int(sys.stdin.readline())
+nlist = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+for _ in range(M):
+    S, E = map(int, sys.stdin.readline().split())
